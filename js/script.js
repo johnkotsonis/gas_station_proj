1c1b6c7e6fd0712b5fd9f497a5c41dee095e2c17
kotsosss <52543341+johnkotsonis@users.noreply.github.com>
2022-04-11 09:35:43 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function log_button(){
+    window.location="https://www.google.com/"
+}
+
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  } 
+
