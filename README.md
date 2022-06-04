522fa4ab50c00c351f7be71548326a60a4cb65e1
kotsosss <johnkotsonis00@gmail.com>
2022-06-04 08:25:15 +0300

    express added

--- README.md
+++
@@ -1,6 +0,0 @@
-# gas_station_proj
-An application for a gas station
-
-
-#how to run
-απλά πάρε το index και άνοιξε live server 
---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+const path = require('path');
+
+dotenv.config({ path: './.env' })
+
+
+const app = express();
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABSE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+
+});
+
+
+const publicDirectory = path.join(__dirname,'./public')
+app.use(express.static(publicDirectory))
+
+app.set('view engine','hbs');
+
+
+db.connect( (error)=>{
+    if(error){
+        console.log(error)
+    }else{
+        console.log("MySQL connected...")
+    }
+    
+})
+
+
+app.get("/", (req,res)=>{
+    // res.send("<h1>test<h1>")
+    res.render("index")
+});
+
+app.get("/register", (req,res)=>{
+    // res.send("<h1>test<h1>")
+    res.render("register")
+});
+
+app.listen(5000,()=>{
+    console.log("testtt")
+})
